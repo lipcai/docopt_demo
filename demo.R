f3b20425b8b678d1d4872343a6321e703c1a72ef
cailh <45726036+lipcai@users.noreply.github.com>
2021-11-19 16:05:27 -0800

    modify the script so that it prints out the value for this new argument
    Copy the `demo.R` script into your `docopt_demo` repo, and then edit it to add a new positional argument that is optional. Also modify the script so that it prints out the value for this new argument in addition to the entire `docopt` object and `docopt` object type. Commit this change and push it to your `docopt_demo` repository.

--- demo.R
+++ demo.R
@@ -2,9 +2,10 @@
 # date: 2020-01-15
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R [--file_path=<file_path>] <arg1> --arg2=<arg2> [--arg3=<arg3>]
 
 Options:
+[--file_path=<file_path>]  Path to the data file
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
@@ -14,3 +15,4 @@ library(docopt)
 opt <- docopt(doc)
 print(opt)
 print(typeof(opt))
+
